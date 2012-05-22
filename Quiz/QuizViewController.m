8822de09180c848acf8689e0d5ded5ae60bfcbf8
RayHightower <rth@wisdomgroup.com>
2012-05-21 22:24:01 -0500

    typos corrected

--- Quiz/QuizViewController.m
+++ Quiz/QuizViewController.m
@@ -14,21 +14,64 @@
 
 @implementation QuizViewController
 
-- (void)viewDidLoad
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Create two arrays and make teh pointers point ot them
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        // Add questions and answers to the arrays
+        [questions addObject:@"What is the square root of nine?"];
+        [answers addObject:@"3"];
+        
+        [questions addObject:@"What is the capital of Illinois?"];
+        [answers addObject:@"Springfield"];
+        
+        [questions addObject:@"What is the answer?"];
+        [answers addObject:@"42"];
+        
+    }
+    // Return the address of the new object
+    return self;
+    
+}
+- (IBAction)showQuestion:(id)sender:(id)sender
+{
+    // Step to the next question
+    currentQuestionIndex++;
+    
+    // Am I past the last question?
+    if (currentQuestionIndex == [questions count]) {
+        
+        // Go back to the first question
+        currentQuestionIndex = 0;
+        
+    }
+    
+    // Get the string at that index in teh euqstions array
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    
+    // Log the string to the console
+    NSLog(@"displaying question: %@", question);
+    
+    //Display the string in teh question field
+    [questionField setText:question];
+    
+    // Clear the answer field
+    [answerField setText:@"???"];
+    
 }
 
-- (void)viewDidUnload
+- (IBAction)showAnswer:(id)sender:(id)sender
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    // What is the answer to the current question?
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    
+    // Display it in the answer field
+    [answerField setText:answer];
 }
 
 @end
